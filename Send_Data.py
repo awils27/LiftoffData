6695ae904bdda51c9b3b62bdace3fe9701e48958
Aiden <68633820+awils27@users.noreply.github.com>
2024-02-28 15:58:10 +1100

    Using seek to restart the messages

--- Send_Data.py
+++ Send_Data.py
@@ -5,8 +5,14 @@ UDP_PORT = 9001
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# Open the file to read the saved UDP messages
-with open("liftoff.bin", "rb") as file:  # Use "rb" mode to read binary data
-    for line in file:
-        # Send each line/message over UDP
-        sock.sendto(line.rstrip(b"\n"), (UDP_IP, UDP_PORT))
+while True:
+    # Open the file to read the saved UDP messages
+    with open("udp_messages.bin", "rb") as file:  # Use "rb" mode to read binary data
+        while True:
+            # Read each line/message from the file
+            for line in file:
+                # Send each line/message over UDP
+                sock.sendto(line.rstrip(b"\n"), (UDP_IP, UDP_PORT))
+            
+            # Once end of file is reached, go back to the beginning
+            file.seek(0)
